d0802c6777c25c1593d44ca9bea7ada193d98ed5
tlsrhkddus96 <tlsrhkddus96@users.noreply.github.com>
2021-08-11 19:58:43 +0900

    SecurityDemo

---
+++ src/main/java/org/zerock/club2/Club2Application.java
@@ -0,0 +1,13 @@
+package org.zerock.club2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Club2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Club2Application.class, args);
+	}
+
+}
