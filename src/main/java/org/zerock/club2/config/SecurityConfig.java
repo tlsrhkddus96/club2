3d79e18f4c39d66ba529e0b7c60748b7bce5e1ee
tlsrhkddus96 <tlsrhkddus96@users.noreply.github.com>
2021-08-23 19:47:58 +0900

    Security(GoogleAPI)

--- src/main/java/org/zerock/club2/config/SecurityConfig.java
+++ src/main/java/org/zerock/club2/config/SecurityConfig.java
@@ -4,14 +4,20 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.zerock.club2.security.handler.ClubLoginSuccessHandler;
+import org.zerock.club2.security.service.ClubUserDetailsService;
 
 @Configuration
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private ClubUserDetailsService userDetailsService;
 
     @Bean
     PasswordEncoder passwordEncoder(){
@@ -21,15 +27,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
 
-        http.authorizeRequests()
-                .antMatchers("/sample/all").permitAll()
-                .antMatchers("/sample/member").hasRole("USER");
+//        http.authorizeRequests()
+//                .antMatchers("/sample/all").permitAll()
+//                .antMatchers("/sample/member").hasRole("USER");
 
         http.formLogin();
         http.csrf().disable();
 
-        http.oauth2Login();
+        http.oauth2Login().successHandler(successHandler());
+        http.rememberMe().tokenValiditySeconds(60*60*24*7).userDetailsService(userDetailsService);
 
+
+    }
+
+    @Bean
+    public ClubLoginSuccessHandler successHandler(){
+        return new ClubLoginSuccessHandler(passwordEncoder());
     }
 
 
--- src/main/java/org/zerock/club2/controller/SampleController.java
+++ src/main/java/org/zerock/club2/controller/SampleController.java
@@ -1,6 +1,7 @@
 package org.zerock.club2.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +13,12 @@ import org.zerock.club2.security.dto.ClubAuthMemberDTO;
 @RequestMapping("/sample")
 public class SampleController {
 
+    @PreAuthorize("permitAll()")
     @GetMapping("/all")
     public void exAll(){
         log.info("exAll.....");
     }
+
 
     @GetMapping("/member")
     public void exMember(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMember){
@@ -26,9 +29,19 @@ public class SampleController {
 
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public void exAdmin(){
         log.info("exAdmin.....");
+    }
+
+    @PreAuthorize("#clubAuthMember != null && #clubAuthMember.username eq \"user95@zerock.org\"")
+    @GetMapping("/exOnly")
+    public String exMemberOnly(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMember){
+        log.info("exMemberOnly");
+        log.info(clubAuthMember);
+
+        return "/sample/admin";
     }
 
 }
